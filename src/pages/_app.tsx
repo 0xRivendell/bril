ce6d2bab5f1e0f0b4a90140412a1ac9a39b9c8d6
Shlok D. Mange <shlok_mange@protonmail.com>
2023-11-13 14:21:54 +0530

    dynamic widget insert.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { DynamicContextProvider } from '@dynamic-labs/sdk-react-core'
+import { EthereumWalletConnectors } from '@dynamic-labs/ethereum'
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <DynamicContextProvider settings={{
+      environmentId: `846dbf00-bb60-4cf1-9797-58b6a1208028`,
+      walletConnectors: [EthereumWalletConnectors]
+    }}>
+
+      <Component {...pageProps} />
+    </DynamicContextProvider>
+  )
 }
