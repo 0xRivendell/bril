38cbeae619befd552d3952f47611ea9359deca5f
Shlok D. Mange <shlok_mange@protonmail.com>
2023-11-20 16:51:11 +0530

    header done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,13 +4,18 @@ import { DynamicWidget } from '@dynamic-labs/sdk-react-core'
 const Home: NextPage = () => {
 	return(
 		<div className='bg-cover bg-center h-screen w-screen' style={{ backgroundImage: `url('/bril_background.svg')` }}>
+			{/* horizontal stack */}
 			<div className='flex justify-between px-8 py-8'>
-				<h1 style={{ fontFamily: 'Major Mono Display'}} className='text-6xl'>
-					R
-				</h1>
-				<div>
-					<DynamicWidget/>
+				<div className='flex items-center space-x-4'>
+					<h1 style={{ fontFamily: 'Major Mono Display'}} className='text-6xl'>
+						R
+					</h1>
+					<p className='text-3xl font-mono'>0xRivendell</p>
 				</div>
+			</div>
+			{/* body */}
+			<div>
+				
 			</div>
 		</div>
 	)
