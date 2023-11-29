b86d78435ddc7386302d9bc2243273e518b93219
Shlok D. Mange <shlok_mange@protonmail.com>
2023-11-29 11:55:15 +0530

    chakra init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,8 @@ import type { AppProps } from 'next/app'
 import { DynamicContextProvider } from '@dynamic-labs/sdk-react-core'
 import { EthereumWalletConnectors } from '@dynamic-labs/ethereum'
 
+import { ChakraProvider } from '@chakra-ui/react'
+
 import '@fontsource/major-mono-display'
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -12,8 +14,9 @@ export default function App({ Component, pageProps }: AppProps) {
       environmentId: `846dbf00-bb60-4cf1-9797-58b6a1208028`,
       walletConnectors: [EthereumWalletConnectors]
     }}>
-
-      <Component {...pageProps} />
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </DynamicContextProvider>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from 'next'
-import { DynamicWidget } from '@dynamic-labs/sdk-react-core'
+import { DynamicWidget, useDynamicContext } from '@dynamic-labs/sdk-react-core'
 import { useState } from 'react'
 
 const Home: NextPage = () => {
@@ -7,7 +7,18 @@ const Home: NextPage = () => {
 	const [isLoggedIntoWallet, setIsLoggedIntoWallet] = useState(false)
 	const [currentResponseTime, setCurrentResponseTime] = useState(0)
 
-	const calculateResponseTime = async () => {
+	const [currPrompt, setCurrPrompt] = useState('')
+
+	const { 
+		connectedWallets, 
+		isAuthenticated, 
+		isFullyConnected, 
+		primaryWallet,
+		network,
+		user
+	} = useDynamicContext()
+
+	const calculateResponseTime = async () : Promise<number> => {
 		const startTime = performance.now()
 		try {
 
@@ -18,31 +29,53 @@ const Home: NextPage = () => {
 		const endTime = performance.now()
 		const responseTime = endTime - startTime
 		setCurrentResponseTime(responseTime)
+		return responseTime
 	}
 
-	return(
-		<div className='bg-cover bg-center h-screen w-screen' style={{ backgroundImage: `url('/bril_background.svg')` }}>
-			{/* horizontal stack */}
-			<div className='flex justify-between px-8 py-8'>
-				<div className='flex items-center space-x-4'>
-					<h1 style={{ fontFamily: 'Major Mono Display'}} className='text-6xl'>
-						R
-					</h1>
-					<p className='text-3xl font-mono'>0xRivendell</p>
+	if(user){
+		return(
+			<div className='bg-cover bg-center h-screen w-screen' style={{ backgroundImage: `url('/bril_background.svg')` }}>
+				{/* horizontal stack */}
+				<div className='flex justify-between px-8 py-8'>
+					<div className='flex items-center space-x-4'>
+						<h1 style={{ fontFamily: 'Major Mono Display'}} className='text-6xl'>
+							R
+						</h1>
+						<p className='text-3xl font-mono'>0xRivendell</p>
+					</div>
+				</div>
+				{/* body */}
+				<div className='px-8 py-8 space-y-4'>
+					<div className='flex items-center justify-center'>
+						<h1 className='text-center font-mono text-9xl'>bril</h1>
+					</div>
+					<div className='flex items-center justify-center'>
+						<DynamicWidget />
+					</div>
+				</div>
+				<footer className="fixed inset-x-0 bottom-0 text-center py-16 font-mono">
+					engineered by <span className='underline'>0xRivendell</span>
+				</footer>
+			</div>
+		)
+	}
+
+	return (
+		<div className='bg-cover bg-center h-screen w-screen font-mono' style={{ backgroundImage: `url('/bril_background.svg')` }}>
+			<div className='px-8 py-4'>
+				<h1>bril.</h1>
+			</div>
+			<div className='flex px-8'>
+				{/* input box + api response goes here */}
+				<div className='flex w-1/2 space-x-4'>
+					<input type="text" className='bg-gray-700'/>
+					<button className='bg-white text-black py-2 px-4'>send</button>
+				</div>
+				{/* user history goes here */}
+				<div>
+
 				</div>
 			</div>
-			{/* body */}
-			<div className='px-8 py-8 space-y-4'>
-				<div className='flex items-center justify-center'>
-					<h1 className='text-center font-mono text-9xl'>bril</h1>
-				</div>
-				<div className='flex items-center justify-center'>
-					<DynamicWidget />
-				</div>
-			</div>
-			<footer className="fixed inset-x-0 bottom-0 text-center py-16 font-mono">
-            	engineered by <span className='underline'>0xRivendell</span>
-        	</footer>
 		</div>
 	)
 }
