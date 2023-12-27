eb357fe2383b39796ebe8f6039051dcb3c91a088
Shlok D. Mange <shlok_mange@protonmail.com>
2023-12-28 00:29:12 +0530

    added custom evm networks

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,13 +8,64 @@ import { ChakraProvider } from '@chakra-ui/react'
 import '@fontsource/major-mono-display'
 import { EthereumWalletConnectors } from '@dynamic-labs/ethereum'
 
+const evmNetworks = [
+  {
+    blockExplorerUrls: ['https://etherscan.io/'],
+    chainId: 1,
+    chainName: 'Ethereum Mainnet',
+    iconUrls: ['https://app.dynamic.xyz/assets/networks/eth.svg'],
+    name: 'Ethereum',
+    nativeCurrency: {
+      decimals: 18,
+      name: 'Ether',
+      symbol: 'ETH',
+    },
+    networkId: 1,
+    
+    rpcUrls: ['https://mainnet.infura.io/v3/'],
+    vanityName: 'ETH Mainnet',
+  },
+{
+    blockExplorerUrls: ['https://etherscan.io/'],
+    chainId: 5,
+    chainName: 'Ethereum Goerli',
+    iconUrls: ['https://app.dynamic.xyz/assets/networks/eth.svg'],
+    name: 'Ethereum',
+    nativeCurrency: {
+      decimals: 18,
+      name: 'Ether',
+      symbol: 'ETH',
+    },
+    networkId: 5,
+    rpcUrls: ['https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161'],
+    
+    vanityName: 'Goerli',
+  },
+  {
+    blockExplorerUrls: ['https://polygonscan.com/'],
+    chainId: 137,
+    chainName: 'Matic Mainnet',
+    iconUrls: ["https://app.dynamic.xyz/assets/networks/polygon.svg"],
+    name: 'Polygon',
+    nativeCurrency: {
+      decimals: 18,
+      name: 'MATIC',
+      symbol: 'MATIC',
+    },
+    networkId: 137,
+    rpcUrls: ['https://polygon-rpc.com'],    
+    vanityName: 'Polygon',
+  },
+]
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <DynamicContextProvider
       settings={{
         environmentId: `5229c94a-82e8-4fc6-b2ad-8afca1e656e4`,
         walletConnectors: [EthereumWalletConnectors],
-        walletConnectorExtensions: [EthersExtension]
+        walletConnectorExtensions: [EthersExtension],
+        evmNetworks
       }}
     >
       <ChakraProvider>
